e3facb2e81c2f2fb5679347ccda15ea2a3ecbf73
Maximilian Litteral <Maximilian@Maximilians-MacBook-Pro.local>
2015-06-11 09:36:34 -0700

    Initial Commit

---
+++ TraktKit/TraktKit.h
@@ -0,0 +1,19 @@
+//
+//  TraktKit.h
+//  TraktKit
+//
+//  Created by Maximilian Litteral on 6/11/15.
+//  Copyright (c) 2015 Maximilian Litteral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TraktKit.
+FOUNDATION_EXPORT double TraktKitVersionNumber;
+
+//! Project version string for TraktKit.
+FOUNDATION_EXPORT const unsigned char TraktKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TraktKit/PublicHeader.h>
+
+
