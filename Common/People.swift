d393997b2e3b1c677b93f87956323bb38346239b
Maximilian Litteral <maxlitteral@gmail.com>
2016-02-26 19:28:24 -0800

    - Use proper labelling

--- Common/People.swift
+++ Common/People.swift
@@ -30,7 +30,7 @@ extension TraktManager {
      
      The `crew` object will be broken up into `production`, `art`, `crew`, `costume & make-up`, `directing`, `writing`, `sound`, and `camera` (if there are people for those crew positions). Each of those members will have a `job` and a standard `movie` object.
      */
-    public func getMovieCredits<T: CustomStringConvertible>(movieID id: T, completion: dictionaryCompletionHandler) -> NSURLSessionDataTask? {
+    public func getMovieCredits<T: CustomStringConvertible>(personID id: T, completion: dictionaryCompletionHandler) -> NSURLSessionDataTask? {
         return getCredits(type: WatchedType.Movies, id: id, completion: completion)
     }
     
@@ -39,7 +39,7 @@ extension TraktManager {
      
      The `crew` object will be broken up into `production`, `art`, `crew`, `costume & make-up`, `directing`, `writing`, `sound`, and `camera` (if there are people for those crew positions). Each of those members will have a `job` and a standard `show` object.
      */
-    public func getShowCredits<T: CustomStringConvertible>(showID id: T, completion: dictionaryCompletionHandler) -> NSURLSessionDataTask? {
+    public func getShowCredits<T: CustomStringConvertible>(personID id: T, completion: dictionaryCompletionHandler) -> NSURLSessionDataTask? {
         return getCredits(type: WatchedType.Shows, id: id, completion: completion)
     }
     
