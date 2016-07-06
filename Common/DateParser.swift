c111cda859bae1e972998b636a33ddafbbecfe5f
Maximilian Litteral <maxlitteral@gmail.com>
2016-07-06 09:03:44 -0700

    - Improve date parsing

--- Common/DateParser.swift
+++ Common/DateParser.swift
@@ -8,16 +8,9 @@
 
 import Foundation
 
+// Internal
 internal let calendar = NSCalendar.currentCalendar()
 internal let dateFormatter = NSDateFormatter()
-
-internal let ISO8601DateFormatter: NSDateFormatter = {
-    let dateFormatter = NSDateFormatter()
-    let enUSPOSIXLocale = NSLocale(localeIdentifier: "en_US_POSIX")
-    dateFormatter.locale = enUSPOSIXLocale
-    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-    return dateFormatter
-}()
 
 internal extension NSDate {
     
@@ -25,6 +18,27 @@ internal extension NSDate {
     
     class func dateFromString(string: AnyObject?) -> NSDate? {
         guard let dateString = string as? String else { return nil }
+        
+        let count = dateString.characters.count
+        if count <= 10 {
+            ISO8601DateFormatter.dateFormat = "yyyy-MM-dd"
+        }
+        else {
+            ISO8601DateFormatter.dateFormat = UTCFormat
+        }
+        
         return ISO8601DateFormatter.dateFromString(dateString)
     }
 }
+
+// Private
+
+private let UTCFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+
+private let ISO8601DateFormatter: NSDateFormatter = {
+    let dateFormatter = NSDateFormatter()
+    let enUSPOSIXLocale = NSLocale(localeIdentifier: "en_US_POSIX")
+    dateFormatter.locale = enUSPOSIXLocale
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    return dateFormatter
+}()
