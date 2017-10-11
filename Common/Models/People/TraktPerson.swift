14a68b088884ef621135439c373e7b79d2434784
Attila Hegedüs <attilahegedus90@gmail.com>
2017-10-11 19:42:09 +0200

    Handle exception when parsing a person’s data with empty string specified as homepage

--- Common/Models/People/TraktPerson.swift
+++ Common/Models/People/TraktPerson.swift
@@ -20,4 +20,31 @@ public struct Person: Codable {
     public let death: Date?
     public let birthplace: String?
     public let homepage: URL?
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case ids
+        
+        case biography
+        case birthday
+        case death
+        case birthplace
+        case homepage
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: CodingKeys.name)
+        ids = try container.decode(ID.self, forKey: CodingKeys.ids)
+        biography = try container.decodeIfPresent(String.self, forKey: CodingKeys.biography)
+        birthday = try container.decodeIfPresent(Date.self, forKey: CodingKeys.birthday)
+        death = try container.decodeIfPresent(Date.self, forKey: CodingKeys.death)
+        birthplace = try container.decodeIfPresent(String.self, forKey: CodingKeys.birthplace)
+        do {
+            homepage = try container.decodeIfPresent(URL.self, forKey: CodingKeys.homepage)
+        } catch {
+            homepage = nil
+        }
+    }
+    
 }
