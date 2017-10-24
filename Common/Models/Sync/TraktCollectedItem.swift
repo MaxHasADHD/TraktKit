e2cba931b6c4333157af09f67ef8b4baca312aa5
Maximilian Litteral <maxlitteral@gmail.com>
2017-10-24 06:29:16 -0400

    Max/movie fixes (#13)
    * Fix property access control
    
    * Fix collected item parsing collection date for movies

--- Common/Models/Sync/TraktCollectedItem.swift
+++ Common/Models/Sync/TraktCollectedItem.swift
@@ -17,10 +17,37 @@ public struct TraktCollectedItem: Codable {
     public var seasons: [TraktCollectedSeason]?
     
     enum CodingKeys: String, CodingKey {
-        case lastCollectedAt = "last_collected_at" // Can be last_collected_at / collected_at though :/
+        case lastCollectedAt = "last_collected_at" // Can be last_collected_at / collected_at though
+        case movieLastCollectAt = "collected_at"
         case movie
         case show
         case seasons
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        do {
+            self.lastCollectedAt = try container.decode(Date.self, forKey: .lastCollectedAt)
+        } catch {
+            self.lastCollectedAt = try container.decode(Date.self, forKey: .movieLastCollectAt)
+        }
+        movie = try container.decodeIfPresent(TraktMovie.self, forKey: .movie)
+        show = try container.decodeIfPresent(TraktShow.self, forKey: .show)
+        seasons = try container.decodeIfPresent([TraktCollectedSeason].self, forKey: .seasons)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        if movie != nil {
+            try container.encode(lastCollectedAt, forKey: .movieLastCollectAt)
+        } else {
+            try container.encode(lastCollectedAt, forKey: .lastCollectedAt)
+        }
+        try container.encodeIfPresent(movie, forKey: .movie)
+        try container.encodeIfPresent(show, forKey: .show)
+        try container.encodeIfPresent(seasons, forKey: .seasons)
     }
 }
 
