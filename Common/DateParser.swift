541c49e897cf89cbf051f269653ed707b7502b95
Maximilian Litteral <maxlitteral@gmail.com>
2018-02-26 04:54:47 -0500

    Fix parsing dates
    Some time zones expect the day to come before the month, and would return nil otherwise

--- Common/DateParser.swift
+++ Common/DateParser.swift
@@ -22,26 +22,32 @@ internal extension Date {
     
     enum DateParserError: Error {
         case failedToParseDateFromString(String)
+        case typeUnhandled(Any?)
     }
     
     // MARK: - Class
     
     static func dateFromString(_ string: Any?) throws -> Date {
-        guard let dateString = string as? String else { throw DateParserError.failedToParseDateFromString("\(String(describing: string))") }
-        
-        let count = dateString.count
-        if count <= 10 {
-            ISO8601DateFormatter.dateFormat = "yyyy-MM-dd"
-        } else if count == 23 {
-            ISO8601DateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss ZZZ"
-        } else {
-            ISO8601DateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        }
-        
-        if let date = ISO8601DateFormatter.date(from: dateString) {
+        if let dateString = string as? String {
+
+            let count = dateString.count
+            if count <= 10 {
+                ISO8601DateFormatter.dateFormat = "yyyy-MM-dd"
+            } else if count == 23 {
+                ISO8601DateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss ZZZ"
+            } else {
+                ISO8601DateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+            }
+
+            if let date = ISO8601DateFormatter.date(from: dateString) {
+                return date
+            } else {
+                throw DateParserError.failedToParseDateFromString("String to parse: \(dateString), date format: \(ISO8601DateFormatter.dateFormat)")
+            }
+        } else if let date = string as? Date {
             return date
         } else {
-            throw DateParserError.failedToParseDateFromString(dateString)
+            throw DateParserError.typeUnhandled(string)
         }
     }
     
@@ -65,5 +71,6 @@ private let ISO8601DateFormatter: DateFormatter = {
     let enUSPOSIXLocale = Locale(identifier: "en_US_POSIX")
     dateFormatter.locale = enUSPOSIXLocale
     dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    dateFormatter.timeZone = TimeZone(abbreviation: "GMT") ?? TimeZone.current
     return dateFormatter
 }()
