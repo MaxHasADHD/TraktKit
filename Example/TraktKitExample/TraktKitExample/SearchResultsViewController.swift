fa71c7561637640e405b3937e34f4fc3d84a6d66
Maximilian Litteral <maximilian@intrepid.io>
2019-01-17 12:22:10 -0500

    Add example project

---
+++ Example/TraktKitExample/TraktKitExample/SearchResultsViewController.swift
@@ -0,0 +1,104 @@
+//
+//  SearchResultsViewController.swift
+//  TraktKitExample
+//
+//  Created by Litteral, Maximilian on 1/17/19.
+//  Copyright © 2019 Maximilian Litteral. All rights reserved.
+//
+
+import UIKit
+import TraktKit
+
+final class SearchResultsViewController: UITableViewController {
+
+    // MARK: - Properties
+    private var shows: [TraktShow] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+
+    let query: String
+
+    // MARK: - Lifecycle
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        fatalError()
+    }
+
+    init(query: String) {
+        self.query = query
+        super.init(style: .plain)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Search"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        TraktManager.sharedManager.search(query: query, types: [.show], extended: [.Full], pagination: nil, filters: nil, fields: nil) { [weak self] result in
+            switch result {
+            case .success(let objects):
+                DispatchQueue.main.async {
+                    self?.shows = objects.compactMap { $0.show }
+                }
+            case .error(let error):
+                print("Failed to get search results: \(String(describing: error?.localizedDescription))")
+            }
+        }
+    }
+
+    // MARK: - UITableViewDataSource
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shows.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let show = shows[indexPath.row]
+        cell.textLabel?.text = show.title
+        return cell
+    }
+
+    // MARK: - UITableViewDelegate
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        func dataOrUnknown(_ input: String?) -> String {
+            return input ?? "Unknown"
+        }
+
+        func numberOrUnknown(_ input: Int?) -> String {
+            return input != nil ? "\(input!)" : "Unknown"
+        }
+
+        let show = shows[indexPath.row]
+        let showInfo = """
+        Show name: \(show.title),
+        Year: \(numberOrUnknown(show.year)),
+        Certification: \(dataOrUnknown(show.certification)),
+        Country: \(dataOrUnknown(show.country)),
+        Day of week airing: \(dataOrUnknown(show.airs?.day)),
+        """
+
+        let alertController = UIAlertController(title: "Info", message: showInfo, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
---
+++ Example/TraktKitExample/TraktKitExample/ViewController.swift
@@ -0,0 +1,145 @@
+//
+//  ViewController.swift
+//  TraktKitExample
+//
+//  Created by Litteral, Maximilian on 1/11/19.
+//  Copyright © 2019 Maximilian Litteral. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+import TraktKit
+
+final class ViewController: UIViewController {
+
+    // MARK: - Properties
+
+    private let stackView = UIStackView()
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        setup()
+    }
+
+    // MARK: - Actions
+
+    private func presentLogIn() {
+        guard let oauthURL = TraktManager.sharedManager.oauthURL else { return }
+
+        let traktAuth = SFSafariViewController(url: oauthURL)
+        present(traktAuth, animated: true, completion: nil)
+    }
+
+    private func signOut() {
+        TraktManager.sharedManager.accessToken = nil
+        refreshUI()
+    }
+
+    private func presentUserInfo() {
+        let profileViewController = TraktProfileViewController()
+        show(profileViewController, sender: self)
+    }
+
+    private func searchForShow() {
+        let alertController = UIAlertController(title: "Show name?", message: nil, preferredStyle: .alert)
+        alertController.addTextField { textField in
+            if #available(iOS 11.0, *) {
+                textField.smartQuotesType = .no // Smart quotes effect search results, always turn off.
+            }
+            textField.autocorrectionType = .no
+            textField.returnKeyType = .search
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+
+        let searchAction = UIAlertAction(title: "Search", style: .default) { [weak navigationController] _ in
+            guard let query = alertController.textFields?.first?.text else { return }
+            let searchResultsViewController = SearchResultsViewController(query: query)
+            navigationController?.pushViewController(searchResultsViewController, animated: true)
+        }
+        alertController.addAction(searchAction)
+
+        present(alertController, animated: true, completion: nil)
+    }
+
+    // MARK: Setup
+
+    private func setup() {
+        self.title = "TraktKit"
+        stackView.alignment = .center
+        stackView.distribution = .equalCentering
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+
+        stackView.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        refreshUI()
+        setupObservers()
+    }
+
+    private func setupObservers() {
+        NotificationCenter.default.addObserver(forName: .TraktSignedIn, object: nil, queue: nil) { [weak self] _ in
+            self?.dismiss(animated: true, completion: nil) // Dismiss the SFSafariViewController
+            self?.refreshUI()
+        }
+    }
+
+    private func refreshUI() {
+        func createButton(title: String, action: @escaping (UIButton) -> Void) -> UIButton {
+            let button = ClosureButton(type: .system)
+            button.setTitle(title, for: .normal)
+            button.action = action
+            return button
+        }
+
+        stackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+
+        var views = [UIView]()
+
+        if TraktManager.sharedManager.isSignedIn {
+            let signInButton = createButton(title: "Sign Out", action: { [weak self] _ in self?.signOut() })
+            views.append(signInButton)
+
+            let presentLoginButton = createButton(title: "View Profile") { [weak self] _ in
+                self?.presentUserInfo()
+            }
+            views.append(presentLoginButton)
+
+            let showSearchButton = createButton(title: "Search for Show") { [weak self] _ in
+                self?.searchForShow()
+            }
+            views.append(showSearchButton)
+        } else {
+            let signInButton = createButton(title: "Sign In", action: { [weak self] _ in self?.presentLogIn() })
+            views.append(signInButton)
+        }
+
+        views.forEach { stackView.addArrangedSubview($0) }
+    }
+}
+
+final class ClosureButton: UIButton {
+    var action: ((UIButton) -> Void)?
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        addTarget(self, action: #selector(didTouchUpInside(_:)), for: .touchUpInside)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addTarget(self, action: #selector(didTouchUpInside(_:)), for: .touchUpInside)
+    }
+
+    @objc private func didTouchUpInside(_ sender: UIButton) {
+        action?(sender)
+    }
+}
