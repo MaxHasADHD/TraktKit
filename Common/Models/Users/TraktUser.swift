44f49c9690ee995e12badec454b6491c6a6812c2
Moritz Sternemann <moritz@strnmn.me>
2019-12-11 17:30:14 +0100

    Add slug and avatar to user

--- Common/Models/Users/TraktUser.swift
+++ Common/Models/Users/TraktUser.swift
@@ -9,13 +9,14 @@
 import Foundation
 
 public struct User: Codable {
-    
+
     // Min
     public let username: String?
     public let isPrivate: Bool
     public let name: String?
     public let isVIP: Bool?
     public let isVIPEP: Bool?
+    public let ids: IDs
     
     // Full
     public let joinedAt: Date?
@@ -23,6 +24,7 @@ public struct User: Codable {
     public let about: String?
     public let gender: String?
     public let age: Int?
+    public let images: Images?
     
     // VIP
     public let vipOG: Bool?
@@ -34,12 +36,26 @@ public struct User: Codable {
         case name
         case isVIP = "vip"
         case isVIPEP = "vip_ep"
+        case ids
         case joinedAt = "joined_at"
         case location
         case about
         case gender
         case age
+        case images
         case vipOG = "vip_og"
         case vipYears = "vip_years"
+    }
+
+    public struct IDs: Codable {
+        public let slug: String
+    }
+
+    public struct Images: Codable {
+        public let avatar: Image
+    }
+
+    public struct Image: Codable {
+        public let full: String
     }
 }
