67eff9b6194b0e5eb607852cdb8953ea2c50c681
Zach Knox <zmknox@gmail.com>
2020-03-14 10:46:22 -0400

    Swift PM Support

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TraktKit",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
+    products: [
+        .library(
+            name: "TraktKit",
+            targets: ["TraktKit"]),
+    ],
+    targets: [
+        .target(
+            name: "TraktKit",
+            dependencies: [],
+            path: "Common"
+            )
+    ],
+    swiftLanguageVersions: [.v5]
+)
